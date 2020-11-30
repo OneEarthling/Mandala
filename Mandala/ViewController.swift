86822e1f95de3bdf06698dc1d183308d0cb4c012
nadya <kazynkina@keysystems.ru>
2020-11-30 10:00:42 +0300

    Initial Commit

---
+++ Mandala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by User on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
